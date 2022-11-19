3dc0cad557e3ee63cfaf2529fb4d1f46bec1f8a7
Chantepierre <lucas@documents.design>
2022-11-19 18:57:07 +0100

    Adds canvas bg

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { computed, onMounted, ref, Ref } from "vue";
 import { data } from "./data";
+import ShootingStars from "./ShootingStars.vue";
 
 const currentLang: Ref<"fr" | "en"> = ref("fr");
 
@@ -26,81 +27,87 @@ let trigger = () => {
   window.removeEventListener('touchstart', trigger);
 };
 
+const ws = ref(window.innerWidth);
+
 onMounted(() => {
   window.addEventListener('mousemove', trigger);
   window.addEventListener('touchstart', trigger);
+  window.addEventListener('resize', () => {
+    ws.value = window.innerWidth;
+  });
 });
 </script>
 
 <template>
   <div>
-  <div class="cv">
-    <h2>Lucas Sifoni</h2>
-    <header class="columns">
-      <div class="header-left">
-        <p v-html="t.intro"></p>
-      </div>
-      <div class="header-right">
-        <button class="lang-button" @click="switchLang">{{ t.other_lang }}</button>
-        <p>contact<span>{{ shownMail }}</span></p>
-      </div>
-    </header>
-    <div class="columns">
-      <main>
-        <h3 id="collabs" v-html="t.titles.collab"></h3>
-        <p>
-        <ul class="dates">
-          <li v-for="item in t.collabs">
-            {{ item.start }} &mdash; {{ item.end }} : <span v-html="item.main"></span>
-            <p class="small" v-html="item.details"></p>
-          </li>
-        </ul>
-        </p>
-
-        <h3 id="projects" v-html="t.titles.projects"></h3>
-        <p class="small" v-html="t.projects_intro"></p>
-        <div v-for="list in t.projects">
-          <h4 class="year">{{ list.year }}</h4>
-          <ul class="project-list">
-            <li v-for="project in list.content">
-              <span v-html="project.intro"></span>
-              <span class="tags">
-                <div class="tag" :class="tag.key" v-for="tag in project.tags">
-                  {{ tag.value[currentLang] }}
-                </div>
-              </span>
+    <ShootingStars :key="ws"></ShootingStars>
+    <div class="cv">
+      <h2>Lucas Sifoni</h2>
+      <header class="columns">
+        <div class="header-left">
+          <p v-html="t.intro"></p>
+        </div>
+        <div class="header-right">
+          <button class="lang-button" @click="switchLang">{{ t.other_lang }}</button>
+          <p>contact<span>{{ shownMail }}</span></p>
+        </div>
+      </header>
+      <div class="columns">
+        <main>
+          <h3 id="collabs" v-html="t.titles.collab"></h3>
+          <p>
+          <ul class="dates">
+            <li v-for="item in t.collabs">
+              {{ item.start }} &mdash; {{ item.end }} : <span v-html="item.main"></span>
+              <p class="small" v-html="item.details"></p>
             </li>
           </ul>
-        </div>
-      </main>
-      <aside>
-        <h3 id="tech" v-html="t.titles.tech"></h3>
-        <p v-html="t.tech.current_stack"></p>
-        <ul>
-          <li v-for="item in t.tech.stack" v-html="item"></li>
-        </ul>
-        <p v-html="t.tech.desired_stack"></p>
-        <ul>
-          <li v-for="item in t.tech.desired_stack_items" v-html="item"></li>
-        </ul>
-        <h3 id="formation" v-html="t.titles.form"></h3>
-        <ul class="dates">
-          <li v-for="item in t.formation">
-            {{ item.start }} &mdash; {{ item.end }} : {{ item.title }}
-            <p class="small" v-html="item.details"></p>
-          </li>
-        </ul>
-        <h3 id="oss" v-html="t.titles.oss"></h3>
-        <p v-html="t.oss.title"></p>
-        <ul>
-          <li v-for="item in t.oss.list" v-html="item"></li>
-        </ul>
-        <h3 id="stars" v-html="t.titles.stars"></h3>
-        <p v-html="t.stars"></p>
-      </aside>
+          </p>
+
+          <h3 id="projects" v-html="t.titles.projects"></h3>
+          <p class="small" v-html="t.projects_intro"></p>
+          <div v-for="list in t.projects">
+            <h4 class="year">{{ list.year }}</h4>
+            <ul class="project-list">
+              <li v-for="project in list.content">
+                <span v-html="project.intro"></span>
+                <span class="tags">
+                  <div class="tag" :class="tag.key" v-for="tag in project.tags">
+                    {{ tag.value[currentLang] }}
+                  </div>
+                </span>
+              </li>
+            </ul>
+          </div>
+        </main>
+        <aside>
+          <h3 id="tech" v-html="t.titles.tech"></h3>
+          <p v-html="t.tech.current_stack"></p>
+          <ul>
+            <li v-for="item in t.tech.stack" v-html="item"></li>
+          </ul>
+          <p v-html="t.tech.desired_stack"></p>
+          <ul>
+            <li v-for="item in t.tech.desired_stack_items" v-html="item"></li>
+          </ul>
+          <h3 id="formation" v-html="t.titles.form"></h3>
+          <ul class="dates">
+            <li v-for="item in t.formation">
+              {{ item.start }} &mdash; {{ item.end }} : {{ item.title }}
+              <p class="small" v-html="item.details"></p>
+            </li>
+          </ul>
+          <h3 id="oss" v-html="t.titles.oss"></h3>
+          <p v-html="t.oss.title"></p>
+          <ul>
+            <li v-for="item in t.oss.list" v-html="item"></li>
+          </ul>
+          <h3 id="stars" v-html="t.titles.stars"></h3>
+          <p v-html="t.stars"></p>
+        </aside>
+      </div>
     </div>
   </div>
-</div>
 </template>
 
 <style lang="scss">
@@ -111,10 +118,12 @@ onMounted(() => {
 
 .mobile-menu {
   display: none;
+
   @media screen and (max-width: 960px) {
     display: block;
   }
 }
+
 .lang-button {
   border: 1px solid;
   font-size: 1em;
@@ -206,6 +215,7 @@ onMounted(() => {
       padding: 2px 6px;
       opacity: .5;
       color: black;
+
       @media (prefers-color-scheme: dark) {
         opacity: 1;
       }
@@ -236,11 +246,13 @@ onMounted(() => {
         background: chocolate;
       }
 
-      &.iot, &.typescript {
+      &.iot,
+      &.typescript {
         background: lightblue;
       }
 
-      &.php, &.wp {
+      &.php,
+      &.wp {
         background: darkblue;
         color: white;
       }
@@ -251,7 +263,8 @@ onMounted(() => {
       }
 
       &.w2p,
-      &.etl, &.ovo2 {
+      &.etl,
+      &.ovo2 {
         background: rgb(128, 126, 126);
         color: white;
       }
@@ -272,6 +285,7 @@ onMounted(() => {
   .header-left {
     flex: 0 0 calc(100% - 40ch - 1em);
     position: relative;
+
     @media screen and (max-width: 960px) {
       flex: 0 0 100%;
       margin-left: 0;
@@ -282,6 +296,7 @@ onMounted(() => {
   .header-right {
     flex: 0 0 40ch;
     margin-left: 1rem;
+
     @media screen and (max-width: 960px) {
       flex: 0 0 100%;
       margin-left: 0;
--- src/ShootingStars.vue
+++ src/ShootingStars.vue
@@ -1,28 +1,87 @@
 <template>
-    <canvas ref="canvasRef"></canvas>
+    <canvas id="starfield" ref="canvas"></canvas>
 </template>
 
 <script setup lang="ts">
 import { onMounted, Ref, ref } from 'vue';
 const canvas: Ref<HTMLCanvasElement | null> = ref(null);
-const ctx : Ref<CanvasRenderingContext2D | null> = ref(null);
+const ctx: Ref<CanvasRenderingContext2D | null> = ref(null);
 
 let lastFrameTime = performance.now();
 const FRAME_TIME = 66;
 
 const draw = () => requestAnimationFrame(() => {
     const now = performance.now();
-    if (now < lastFrameTime + FRAME_TIME) return requestAnimationFrame(draw); 
+    if (now < lastFrameTime + FRAME_TIME) return requestAnimationFrame(draw);
     lastFrameTime = now;
+    do_draw();
 });
 
+let WIDTH = 0;
+let HEIGHT = 0;
+
+const d2r = (d: number) => d * Math.PI / 180;
+
+const makeStars = () => {
+    const out = [];
+    const num = Math.round(Math.random() * 60) + 30;
+    for (let i = 0; i < num; i++) {
+        let x = Math.random() * WIDTH;
+        let h = Math.random() * HEIGHT;
+        let mag = 8 - Math.round(Math.random() * 8) + (Math.random() * 4 * h / HEIGHT);
+        let tw = Math.random() * 360;
+        out.push({
+            c: ['#dfe6e9', '#74b9ff', '#fab1a0', '#ffeaa7'][Math.round(Math.random() * 4)],
+            x,
+            y: h,
+            mag,
+            tw,
+        });
+    }
+    return out;
+};
+
+let stars: { c: string, x: number, y: number, mag: number, tw: number }[] = [];
+
+const a = 0.01;
+let total_angle = 0;
+let originX = 0;
+let originY = 0;
+const do_draw = () => {
+    if (!ctx.value) return;
+    const c = ctx.value;
+    const grd = c.createLinearGradient(0, 0, WIDTH, HEIGHT);
+    grd.addColorStop(0, 'rgba(2,0,36,1)');
+    grd.addColorStop(0.35, 'rgba(79,79,112,1)');
+    grd.addColorStop(1, 'rgba(100,145,154,1)');
+    c.fillStyle = grd;
+    c.fillRect(0, 0, WIDTH, HEIGHT);
+    c.setTransform(1, 0, 0, 1, 0, 0);
+    c.translate(WIDTH * 0.65, HEIGHT * 0.4);
+    c.rotate(d2r(total_angle));
+    total_angle += a;
+    stars.forEach((star) => {
+        c.fillStyle = star.c;
+        c.beginPath();
+        c.arc(star.x - originX, star.y - originY, Math.abs(1 + (star.mag / 10) - (Math.sin(d2r(total_angle + star.tw)) * 1)), 0, d2r(360));
+        c.closePath();
+        c.fill();
+    });
+    c.setTransform(1, 0, 0, 1, 0, 0);
+    draw();
+};
+
 const setupCanvas = () => {
-    const PX_SIZE = 4;
     const c = canvas.value as HTMLCanvasElement;
-    const width = window.innerWidth / PX_SIZE;
-    const height = window.innerHeight / PX_SIZE;
+    const width = window.innerWidth / 1;
+    WIDTH = width;
+    const height = window.innerHeight / 1;
+    HEIGHT = height;
+    originX = WIDTH * 0.65;
+    originY = HEIGHT * 0.4;
     c.width = width;
     c.height = height;
+    stars = makeStars();
     ctx.value = c.getContext("2d");
 };
 
@@ -35,3 +94,16 @@ onMounted(() => {
     }, 100);
 });
 </script>
+
+<style lang="scss">
+#starfield {
+    position: fixed;
+    left: 0;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    width: 100vw;
+    height: 100vh;
+    z-index: -1;
+}
+</style>
